8380558395a6dc9881d64da53c0bca53dbf60e7b
jainak <jain.akshaykr@gmail.com>
2013-05-07 14:03:33 -0400

    first commit

---
+++ CMakeFiles/PF_1D.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/PF_1D.dir/PF_1D.cpp.o"
+  "PF_1D.pdb"
+  "PF_1D"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/PF_1D.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
